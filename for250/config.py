a959b1fecaa3d99962b5f99e599a6cc81ffa5e27
Humbertzhang <504490160@qq.com>
2017-11-24 10:02:37 +0800

    init

---
+++ for250/config.py
@@ -0,0 +1,61 @@
+#coding:utf-8
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+"""
+development configuration
+ -- DEBUG: debug mode
+ -- SQLALCHEMY_DATABASE_URI:
+    -- The database URI that should be used for the connection.
+more connection URI format:
+ -- Postgres:
+    -- postgresql://scott:tiger@localhost/mydatabase
+ -- MySQL:
+    -- mysql://scott:tiger@localhost/mydatabase
+ -- Oracle:
+    -- oracle://scott:tiger@127.0.0.1:1521/sidname
+"""
+class DevelopmentConfig(Config):
+    """development configuration"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
+
+"""
+testing configuration
+ -- TESTING: True
+ -- WTF_CSRF_ENABLED:
+    -- in testing environment, we don't need CSRF enabled
+"""
+class TestingConfig(Config):
+    """testing configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data-test.sqlite")
+    WTF_CSRF_ENABLED = False
+
+# production configuration
+
+class ProductionConfig(Config):
+    """production configuration"""
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+
+config = {
+    "develop": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig
+}
+
---
+++ for250/manage.py
@@ -0,0 +1,163 @@
+"""
+project management
+ -- database management
+    -- python manage.py db init: create migrations folder
+    -- python manage.py db migrate: database migrate
+    -- python manage.py db upgrate: upgrade database
+    -- python manage.py shell
+       >> Role.insert_roles() : create user roles
+ -- add administrator
+    -- python manage.py admin
+       \_admin username:
+       \_admin email:
+       \_admin password:
+ -- add users
+    -- python manage.py adduser
+       \_username:
+       \_password:
+       \_email:
+       \_[1:moderator 2:admin 3:user]:
+ -- run project
+    -- python manage.py runserver
+ -- shell environment
+    -- python manage.py shell
+ -- run your unit tests
+    -- python manage.py test
+"""
+
+import sys
+import os
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+from guisheng_app import db, app
+from guisheng_app.models import User, Role, News, Everydaypic, Article,Image,\
+        Picture, Interaction, Comment, Like, Light, Collect,PostTag,Tag,Suggestion,Permission
+
+# 编码设置
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+
+def make_shell_context():
+    """自动加载环境"""
+    return dict(
+        app = app,
+        db = db,
+        User = User,
+        Role = Role,
+        News = News,
+        Everydaypic = Everydaypic,
+        Article = Article,
+        Picture = Picture,
+        Interaction = Interaction,
+        Comment = Comment,
+        Collect = Collect,
+        Like = Like,
+        Light = Light,
+        PostTag = PostTag,
+        Tag = Tag,
+        Suggestion = Suggestion,
+        Image = Image
+    )
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """run your unit tests"""
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.command
+def admin():
+    """add administrator"""
+    from getpass import getpass
+    username = raw_input("\_admin username: ")
+    email = raw_input("\_admin email: ")
+    password = getpass("\_admin password: ")
+    u = User(
+        email = email,
+        name = username,
+        password = password,
+        role_id = 2
+    )
+    db.session.add(u)
+    db.session.commit()
+    print "<admin user %s add in database>" % username
+
+@manager.command
+def add_test_admin():
+    """add test administrator"""
+    from getpass import getpass
+    username = "test1"
+    email = "test1"
+    password = "test1"
+    u = User(
+        email = email,
+        name = username,
+        password = password,
+        role_id = 2
+    )
+    db.session.add(u)
+    db.session.commit()
+    print "<test admin user %s add in database>" % username
+
+@manager.command
+def adduser():
+    """add user"""
+    from getpass import getpass
+    name = raw_input("\_username: ")
+    email = raw_input("\_email: ")
+    role_id = raw_input("\_[1:moderator 2:admin 3:user]: ")
+    password = getpass("\_password: ")
+    u = User(
+        email = email,
+        name = name,
+        password = password,
+        role_id = role_id
+    )
+    db.session.add(u)
+    db.session.commit()
+    print "<user %s add in database>" % name
+
+@manager.command
+def insert_roles():
+    """
+    insert all Roles in command line
+    -------------------------------
+    User: write & comment
+    Moderator: write & comment & moderate_comments
+    Administrator: full permissions
+    """
+    roles = {
+        'User': (Permission.COMMENT,True),
+        'Moderator': (Permission.COMMENT |
+                      Permission.MODERATE_COMMENTS, False),
+        'Administrator': (Permission.COMMENT | 
+                          Permission.MODERATE_COMMENTS |
+                          Permission.ADMINISTER, False)
+    }
+    for r in roles:
+        role = Role.query.filter_by(name=r).first()
+        if role is None:
+            role = Role(name=r)
+        role.permissions = roles[r][0]
+        role.default = roles[r][1]
+        db.session.add(role)
+    db.session.commit()
+    print 'roles has been inserted!'
+    #return 'roles has been inserted!'
+
+
+if __name__ == '__main__':
+    manager.run()
